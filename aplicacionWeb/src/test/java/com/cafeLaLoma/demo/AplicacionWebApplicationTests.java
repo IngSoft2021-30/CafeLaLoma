108271ad3c95745cb686b96f3edcf2ed24798888
Lida Guerrero <tuchysguer@hotmail.com>
2021-10-02 18:44:17 -0500

    Primer commit

---
+++ aplicacionWeb/src/test/java/com/cafeLaLoma/demo/AplicacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeLaLoma.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
